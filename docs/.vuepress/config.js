e7e7682e494642aabbde367618e492a4ae4e811a
roiding <dingran@ran-ding.ga>
2021-07-13 11:41:12 +0800

    设置编辑此页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,92 +1,97 @@
 ﻿module.exports = {
-    //定义静态资源路径别名
-/*     configureWebpack: {
-        resolve: {
-          alias: {static
-            '@static': ''
+  //定义静态资源路径别名
+  /*     configureWebpack: {
+          resolve: {
+            alias: {static
+              '@static': ''
+            }
           }
-        }
-      }, */
-    plugins: [
-	  ['@vuepress/back-to-top'],
-	  ['@vuepress/docsearch',{
-		  appId: 'XEQSFBL9DS',
-		  apiKey: '25a25110f2901993e844dff36f6449b8',
-		  indexName: 'blog',
-		  placeholder: '搜索',
-		  searchParameters:{
-			  hitsPerPage: 10,
-			  //facetFilters:  "",
-		  }
-	  }],
+        }, */
+  plugins: [
+    ['@vuepress/back-to-top'],
+    ['@vuepress/docsearch', {
+      appId: 'XEQSFBL9DS',
+      apiKey: '25a25110f2901993e844dff36f6449b8',
+      indexName: 'blog',
+      placeholder: '搜索',
+      searchParameters: {
+        hitsPerPage: 10,
+        //facetFilters:  "",
+      }
+    }],
+  ],
+  base: '/',
+  title: '小丁的博客',
+  description: '小丁的博客',
+  head: [
+    ['link', { rel: 'icon', href: '/favicons/favicon.png', type: 'image/png', sizes: '16x16' }]
+  ],
+  port: 9090,
+  extraWatchFiles: [],
+  theme: '@vuepress/theme-default', // 主题
+  themeConfig: {
+    logo: '/favicons/favicon.png',
+    editLink: false, //禁用编辑
+    backToHome: '回到首页',
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '荒废的代码仓库', link: 'https://github.com/maodou38' },
     ],
-    base: '/',
-    title: '小丁的博客',
-    description: '小丁的博客',
-    head: [
-        ['link', { rel: 'icon', href: '/favicons/favicon.png', type: 'image/png', sizes: '16x16'}]
-    ],
-    port: 9090,
-    extraWatchFiles: [],
-    theme: '@vuepress/theme-default', // 主题
-    themeConfig: {
-		repo: 'roiding/roiding.github.io',
-        logo: '/favicons/favicon.png' ,
-		editLink: false, //禁用编辑
-		backToHome: '回到首页',
-        navbar: [
-            { text: '首页', link: '/' },
-            { text: '荒废的代码仓库', link: 'https://github.com/maodou38'},
-          ],
-        sidebar : {
-          '/docker/': [
-             'docker-install.md',
-             'docker-buildx.md',
-             'docker-remote.md',
-             'docker-command.md',
-             'dockerfile-command.md',
-             'docker-compose-command.md',
-             'docker-app.md',
-             'attention.md',
-          ],
-		  '/guide/':[
-            '',
-          ],
-		  '/java/':[
-            'jvm.md',
-            'spring.md',
-			'JPA',
-            'spring-security.md',
-            'springboot.md',
-            'java-connect.md',
-          ],
-		  '/linux/':[
-            'centos.md',
-            'debian.md',
-            'linux.md',
-          ],
-		  '/opencv/':[
-		     'opencv-java.md',
-		  ],
-		  '/sql/':[
-			'sql.md',
-		  ],
-          '/tools/':[
-            'git.md',
-            'intellij.md',
-            'nginx.md',
-          ],
-		  '/vps/':[
-			'euserv.md',
-		  ],
-      '/github/':[
+    sidebar: {
+      '/docker/': [
+        'docker-install.md',
+        'docker-buildx.md',
+        'docker-remote.md',
+        'docker-command.md',
+        'dockerfile-command.md',
+        'docker-compose-command.md',
+        'docker-app.md',
+        'attention.md',
+      ],
+      '/guide/': [
+        '',
+      ],
+      '/java/': [
+        'jvm.md',
+        'spring.md',
+        'JPA',
+        'spring-security.md',
+        'springboot.md',
+        'java-connect.md',
+      ],
+      '/linux/': [
+        'centos.md',
+        'debian.md',
+        'linux.md',
+      ],
+      '/opencv/': [
+        'opencv-java.md',
+      ],
+      '/sql/': [
+        'sql.md',
+      ],
+      '/tools/': [
+        'git.md',
+        'intellij.md',
+        'nginx.md',
+      ],
+      '/vps/': [
+        'euserv.md',
+      ],
+      '/github/': [
         'github.md',
       ],
-          '/':[
-            'resource.md',
-            'attention.md',
-          ],
-        },
-        lastUpdatedText: '上次编辑:'
-    }
+      '/': [
+        'resource.md',
+        'attention.md',
+      ],
+    },
+    lastUpdatedText: '上次编辑:',
+    repo: 'https://github.com/roiding/roiding.github.io',
+    docsRepo: 'https://github.com/roiding/roiding.github.io',
+    docsBranch: 'docs',
+    docsDir: 'docs',
+    editLinkText:'编辑此页',
+    editLinkPattern: ':repo/-/edit/:branch/:path',
+  }
 }
