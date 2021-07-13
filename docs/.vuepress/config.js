1d3bfbd6771d9b2bdb32d6a95fd9432d2cb26aea
roiding <dingran@ran-ding.ga>
2021-07-13 12:43:41 +0800

    目录格式更改为vuepress2的书写要求

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,7 +37,102 @@
       { text: '首页', link: '/' },
       { text: '荒废的代码仓库', link: 'https://github.com/maodou38' },
     ],
-    sidebar: 'auto',
+    sidebar: {
+      '/docker/':[
+        {
+          text: 'Docker笔记',
+          children:[
+            '/docker/docker-install.md',
+            '/docker/docker-buildx.md',
+            '/docker/docker-remote.md',
+            '/docker/docker-command.md',
+            '/docker/dockerfile-command.md',
+            '/docker/docker-compose-command.md',
+            '/docker/docker-app.md',
+            '/docker/attention.md',
+          ],
+        },
+      ],
+      '/guide/':[
+        {
+          text: '目录'
+        },
+      ],
+      '/java/':[
+        {
+          text: 'JAVA笔记',
+          children:[
+            '/java/jvm.md',
+            '/java/spring.md',
+			      '/java/JPA.md',
+            '/java/spring-security.md',
+            '/java/springboot.md',
+            '/java/java-connect.md',
+          ],
+        },
+      ],
+      '/linux/':[
+        {
+          text: 'Linux笔记',
+          children:[
+            '/linux/centos.md',
+            '/linux/debian.md',
+            '/linux/linux.md',
+          ],
+        },
+      ],
+      '/opencv/':[
+        {
+          text: 'OpenCV笔记',
+          children:[
+            '/opencv/opencv-java.md'
+          ],
+        },
+      ],
+      '/sql/':[
+        {
+          text: 'SQL笔记',
+          children:[
+            '/sql/sql.md'
+          ],
+        },
+      ],
+      '/tools/':[
+        {
+          text: '生产力工具笔记',
+          children:[
+            '/tools/git.md',
+            '/tools/intellij.md',
+            '/tools/nginx.md',
+          ],
+        },
+      ],
+      '/vps/':[
+        {
+          text: 'VPS经验',
+          children:[
+            '/vps/euserv.md',
+          ],
+        },
+      ],
+      '/github/':[
+        {
+          text: 'GitHub经验',
+          children:[
+            '/github/github.md',
+          ],
+        },
+      ],
+      '/':[
+        {
+          text: '首页',
+          children:[
+            '/resource.md',
+            '/attention.md',
+          ],
+        },
+      ],
+    },
     lastUpdatedText: '上次编辑:',
     repo: 'https://github.com/roiding/roiding.github.io',
     docsRepo: 'https://github.com/roiding/roiding.github.io',
