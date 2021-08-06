25ffb87800126345eaf4070d65d0118d77e4bdda
roiding <dingran@ran-ding.ga>
2021-08-06 11:21:48 +0800

    bing的请求统计js 暂不单独做插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,7 +39,15 @@
     },
   },
   head: [
-    ['link', { rel: 'icon', href: '/favicons/favicon.png', type: 'image/png', sizes: '16x16' }]
+    ['link', { rel: 'icon', href: '/favicons/favicon.png', type: 'image/png', sizes: '16x16' }],
+    /* bing的请求统计js 暂不单独做插件 */
+    ['script',{},`
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "7vp68ppzv2");
+    `],
   ],
   port: 9090,
   extraWatchFiles: [],
